9104ea81faacab8847c6057507342340ef4f83d9
anisch <8927916+anisch@users.noreply.github.com>
2022-12-01 21:15:23 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .joinToString(";") { it }
+            .split(";;")
+            .maxOf { elf ->
+                elf
+                    .split(";")
+                    .sumOf { it.toInt() }
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .joinToString(";") { it }
+            .split(";;")
+            .map { elf ->
+                elf
+                    .split(";")
+                    .sumOf { it.toInt() }
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
+    check(part1(testInput) == 24000)
     println(part1(input))
+
+    check(part2(testInput) == 45000)
     println(part2(input))
 }
